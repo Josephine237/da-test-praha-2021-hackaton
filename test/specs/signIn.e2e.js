e07596c3d3b08c6a58fbc5ed98edb2bdc5bf0026
Your Name <you@example.com>
2021-11-27 19:05:53 +0100

    Upraven název

--- test/specs/signIn.e2e.js
+++ test/specs/signIn.e2e.js
@@ -1,7 +1,7 @@
 
 describe('Page', () => {
 
-    it('should open page', () => {
+    it('should sign in', () => {
         browser.reloadSession();
         browser.url('/login?back=my-account');
         browser.pause(2000);
@@ -9,6 +9,10 @@ describe('Page', () => {
         const emailField = $('#email');
         const passwordField = $('#passwd');
         const button = $('.icon-lock');
+
+        expect(emailField).toBeDisplayed();
+        expect(passwordField).toBeDisplayed();
+        expect(button).toBeDisplayed();
 
         emailField.setValue('jana.novak@czechitas.cz');
         passwordField.setValue('DA123');
