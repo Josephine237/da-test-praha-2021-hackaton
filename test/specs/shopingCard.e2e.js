fbf72fb565ec897aa687832411985731ba86083c
Your Name <you@example.com>
2021-11-27 17:26:21 +0100

    shoppingCart

---
+++ test/specs/shopingCard.e2e.js
@@ -0,0 +1,20 @@
+describe('Page', () => {
+
+    it('should open page', () => {
+        browser.reloadSession();
+        browser.url('/login?back=my-account');
+        browser.pause(2000);
+
+        const emailField = $('#email');
+        const passwordField = $('#passwd');
+        const button = $('.icon-lock');
+
+        emailField.setValue('jana.novak@czechitas.cz');
+        passwordField.setValue('DA123');
+        button.click();
+
+        const shopingCard = $('.shopping_cart');
+        shopingCard.click();
+    });
+
+});
